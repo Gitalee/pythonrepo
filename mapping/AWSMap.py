50eb1ff8864d3e4afd7d6840355fdfde087e525a
rushi125111 <rushibd97@gmail.com>
2019-12-16 10:48:53 +0530

    16/12

--- mapping/AWSMap.py
+++ mapping/AWSMap.py
@@ -1,40 +1,74 @@
+#pip install pyyaml
+
 import json as js
+import yaml
+
 loadedData =js.loads(open("newmapping.json").read())
-outputData=js.loads(open("D://task1//output//Ec2withEBS.json").read())
+outputData=js.loads(open("C://Users//personal//Desktop//pythonrepo//task1//output//2ec2withebs.json").read())
 servicesNames=[]
 finalArray=[]
-finalDict = {}
+finalGcpArray=[]
+count=0
 for serviceData in outputData:
     dic = {}
     finalDict = {}
+    finalGcpDict = {}
+
     for singleService in serviceData:
         servicesList = singleService.split("::")
         serviceType=servicesList[1]
-        serviceMap = loadedData["ServiceMap"]
+        properties = loadedData["Properties"]
         #print(serviceMap)
         splitedType=servicesList[2].split("'")
         subType=splitedType[0]
-        mapOfService = serviceMap[serviceType]
-        propMap = loadedData['Mappings']
-        if serviceType == 'EC2':
-            specPropMap = propMap['EC2']
-            if subType=='Instance':
-                for main in mapOfService:
-                    finalDict[serviceType]=main
-                    for subMap in mapOfService[main]:
-                        if subType == subMap:
-                            finalDict[subType]=mapOfService[main][subMap]
-                mapOfInstanceType=specPropMap['Instance']['InstanceType']
-                for instanceType in mapOfInstanceType:
-                    if instanceType==serviceData[singleService]['InstanceType']:
-                        finalDict[instanceType]=mapOfInstanceType[instanceType]
-                        finalArray.append(finalDict)
-            elif subType=='Volume':
-                for main in mapOfService:
-                    finalDict[serviceType]=main
-                    for subMap in mapOfService[main]:
-                        if subType == subMap:
-                            finalDict[subType]=mapOfService[main][subMap]
-                            finalArray.append(finalDict)
-print(finalArray)
+        mappings = loadedData['Mappings']
 
+        serviceProperties = serviceData[singleService]
+        propBlankDict = {}
+        prop={}
+        for property in serviceProperties:
+            if property in properties:
+                if property in mappings:
+                    propBlankDict[properties[property]] = mappings[property][serviceProperties[property]]
+                else:propBlankDict[properties[property]]=serviceProperties[property]#####does not have corresponding gcp values
+        Type =""
+        services=loadedData['Services']
+        subservices=loadedData['Subservices']
+        Type+=services[serviceType]+".v1."+subservices[subType]
+        finalGcpDict['name']=Type+str(count)
+        count=count+1
+        finalGcpDict['type']=Type
+        finalGcpDict['properties']=propBlankDict
+        finalGcpArray.append(finalGcpDict)
+
+#print (finalGcpArray)
+tempFileName = "gcpVolume.yaml"
+FinalTempName = "configurationfinalfile.yaml"
+d = {'resources':[]}
+
+with open(tempFileName, 'r') as stream:
+
+        docs = yaml.load_all(stream, Loader=yaml.FullLoader)
+        dictionary = yaml.safe_load(stream)
+        print(type(dictionary))
+        for i in finalGcpArray:
+            resourcesList=list(dictionary['resources'])[0]
+            z=i['properties']
+            zz=resourcesList['properties'].update(z)
+            i['properties']=resourcesList['properties']
+            resourcesList.update(i)
+            dic.append(resourcesList)
+            break
+        d['resources'] = dic
+        print(d)
+        x={'resources': [{'name': 'compute.v1.disk2', 'type': 'compute.v1.disk', 'properties': {'zone': 'us-east-1a', 'machineType': 'm1-micro', 'disks': [{'deviceName': 'boot', 'type': 'PERSISTENT', 'boot': True, 'initializeParams': {'sourceImage': 'projects/debian-cloud/global/images/family/debian-9'}}], 'networkInterfaces': [{'network': 'global/networks/default'}], 'sizeGb': '5'}}, {'name': 'compute.v1.disk2', 'type': 'compute.v1.disk', 'properties': {'zone': 'us-east-1a', 'machineType': 'm1-micro', 'disks': [{'deviceName': 'boot', 'type': 'PERSISTENT', 'boot': True, 'initializeParams': {'sourceImage': 'projects/debian-cloud/global/images/family/debian-9'}}], 'networkInterfaces': [{'network': 'global/networks/default'}], 'sizeGb': '5'}}, {'name': 'compute.v1.disk2', 'type': 'compute.v1.disk', 'properties': {'zone': 'us-east-1a', 'machineType': 'm1-micro', 'disks': [{'deviceName': 'boot', 'type': 'PERSISTENT', 'boot': True, 'initializeParams': {'sourceImage': 'projects/debian-cloud/global/images/family/debian-9'}}], 'networkInterfaces': [{'network': 'global/networks/default'}], 'sizeGb': '5'}}]}
+        print (type(d))
+        print (type(x))
+
+with open(r'configurationfinalfile.yaml', 'w') as file:
+    print (d)
+    dd=d.copy()
+    if x==d:
+        print("hi")
+    documents = yaml.dump(dd, file)
+    print (documents)
